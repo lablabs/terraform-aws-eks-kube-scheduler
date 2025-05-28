3b787d66233a617259d21e184ac8ecbb7dfdccdc
Attila Sovak <attila.sovak@gmail.com>
2025-05-28 07:59:14 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,7 @@
+# IMPORTANT: Add addon specific variables here
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources."
+  nullable    = false
+}
