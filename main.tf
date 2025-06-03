d261ba607a5e1c0a1fc619258f72bf3d56889abb
Attila Sovak <attila.sovak@gmail.com>
2025-06-03 08:21:54 +0200

    fix(kube-sched): rename chart, remove renovate and irsa

--- main.tf
+++ main.tf
@@ -13,17 +13,12 @@ locals {
     namespace = "kube-system"
 
     helm_chart_name    = "kubernetes-scheduler"
-    helm_chart_version = var.helm_chart_version != null ? var.helm_chart_version : var.manifest_target_revision
-    helm_repo_url      = var.helm_repo_url != null ? var.helm_repo_url : "https://github.com/lablabs/terraform-aws-eks-kube-scheduler"
-    argo_enabled       = var.argo_enabled != null ? var.argo_enabled : true
-    argo_source_type   = var.argo_source_type != null ? var.argo_source_type : "helm-directory"
-    argo_source_path   = var.argo_source_path != null ? var.argo_source_path : var.manifest_target_path
-    argo_helm_enabled  = var.argo_helm_enabled != null ? var.argo_helm_enabled : true
-  }
-
-  addon_irsa = {
-    (local.addon.name) = {
-    }
+    helm_chart_version = var.manifest_target_revision
+    helm_repo_url      = "https://github.com/lablabs/terraform-aws-eks-kube-scheduler"
+    argo_enabled       = true
+    argo_source_type   = "helm-directory"
+    argo_source_path   = var.manifest_target_path
+    argo_helm_enabled  = true
   }
 
   addon_values = yamlencode({
